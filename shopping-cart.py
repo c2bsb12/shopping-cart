b70c8c9e88d7081d7649e805ef94be3dc4027112
Sheridan <sdb358@stern.nyu.edu>
2019-06-11 21:39:28 -0400

    print receipt at end

--- shopping-cart.py
+++ shopping-cart.py
@@ -37,19 +37,24 @@ products = [
 #Info capture
 
 total_price = 0
-
+selected_ids = []
 while True:
     selected_id = input("Please input a product identifier:")
     if selected_id == "DONE":
         break
     else:
+        selected_ids.append(selected_id)
+
+#info display
+
+#print(selected_ids)
+
+for selected_id in selected_ids:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0]
         total_price = total_price + matching_product["price"]
         print("SELECTED PRODUCT:" + matching_product["name"] + " " + str(matching_product["price"]))
-
-#info display
-
+     
 
 print("TOTAL PRICE: " + str(total_price))
 
