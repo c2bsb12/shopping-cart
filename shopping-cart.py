b3558572c8a6a857ac5a6e705d852174543d2164
Sheridan <sdb358@stern.nyu.edu>
2019-06-11 20:37:25 -0400

    capture user input

--- shopping-cart.py
+++ shopping-cart.py
@@ -2,9 +2,17 @@
 
 from pprint import pprint
 
+
+
+    
+
+#A grocery store name of your choice
+#A grocery store phone number and/or website URL and/or address of choice
+#The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2019-06-06 11:31 AM)
+#The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $1.50)
+
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price)
-
 
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
@@ -29,6 +37,15 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
+#Info capture
+
+selected_id = input("Please input a product identifier:")
+print(selected_id)
+print(type(selected_id))
+
+
+#info display
+
 print(products)
 pprint(products)
 
@@ -38,11 +55,9 @@ products_count = len(products)
 #print("There are 20 products:")
 
 
-
 print("-------------")
 print(f"There are {products_count} products:")
 print("-------------")
-
 
 
 for item in products:
@@ -52,7 +67,10 @@ for item in products:
     price_usd = to_usd(item['price'])
     print(f"{item['name']} ... {price_usd}")
 
-
+#The total cost of all shopping cart items, formatted as US dollars and cents (e.g. $4.50), calculated as the sum of their prices
+#The amount of tax owed (e.g. $0.39), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
+#The total amount owed, formatted as US dollars and cents (e.g. $4.89), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
+#A friendly message thanking the customer and/or encouraging the customer to shop again
 
 
 # TODO: write some Python code here to produce the desired output
